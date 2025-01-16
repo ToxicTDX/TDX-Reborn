79180401758fc8dec0549f11c37adfa30f532f2b
ToxicTDX <134112867+ToxicTDX@users.noreply.github.com>
2025-01-16 16:27:14 +0530

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,3 +1,3 @@
 if game.PlaceId == 16732694052 then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/ToxicTDX/TDX-Reborn/refs/heads/main/Fisch?token=GHSAT0AAAAAAC4T7S5E55CP44ECXTK5RAI6Z4HJUNA"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ToxicTDX/TDX-Reborn/refs/heads/main/Fisch"))()
 end
